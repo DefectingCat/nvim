95ed4a81bdb5fe68ee67c41cacace391750da1bc
DefectingCat <i@rua.plus>
2024-03-13 09:44:52 +0800

    feat(format): add format toml with prettier

--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -26,6 +26,7 @@ local opts = {
     -- JavaScript
     formatting.prettierd.with({
       prefer_local = "node_modules/.bin",
+      extra_filetypes = { "toml" },
     }),
     -- Lua
     formatting.stylua,
