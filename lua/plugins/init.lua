a1c96d4eddf390e967824268795bde26f75365be
xfy <i@rua.plus>
2024-04-11 17:18:17 +0800

    update diffview

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -445,10 +445,10 @@ local plugins = {
     opts = {
       enhanced_diff_hl = true,
       view = {
-        --[[ merge_tool = {
+        merge_tool = {
           layout = "diff3_mixed",
           disable_diagnostics = true,
-        }, ]]
+        },
       },
       keymaps = {
         view = {
