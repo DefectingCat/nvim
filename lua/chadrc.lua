e4c9b3f8f72a266727c0e023e5d4621eac858a44
xfy <i@rua.plus>
2024-04-17 15:17:47 +0800

    adjust float term style

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -105,11 +105,11 @@ M.ui = {
     sizes = { sp = 0.3, vsp = 0.2 },
     float = {
       relative = "editor",
-      row = 0.25,
-      col = 0.25,
-      width = 0.5,
-      height = 0.5,
-      border = "single",
+      row = 0.04,
+      col = 0.06,
+      width = 0.85,
+      height = 0.85,
+      border = "rounded",
     },
   },
 }
