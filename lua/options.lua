b9332118d2bf631f62861ac9df8ce4f87ac1f7f3
xfy <i@rua.plus>
2024-06-11 13:59:15 +0800

    set tab width

--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,6 @@
 require("nvchad.options")
 
+local TAB_WIDTH = 4
 local opt = vim.opt
 
 vim.g.dap_virtual_text = true
@@ -16,3 +17,6 @@ opt.iskeyword:append("-")
 opt.termguicolors = true -- True color support
 opt.autoindent = true --- Good auto indent
 opt.scrolloff = 3
+opt.tabstop = TAB_WIDTH
+opt.shiftwidth = TAB_WIDTH
+opt.expandtab = true
