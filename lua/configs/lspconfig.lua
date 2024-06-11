514f5f7302a7e34d44a660ed6a0554135b9998cc
xfy <i@rua.plus>
2024-06-11 09:21:14 +0800

    format lspconfig

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -6,11 +6,11 @@ local lspconfig = require("lspconfig")
 local util = require("lspconfig/util")
 
 local function organize_imports()
-  local params = {
-    command = "_typescript.organizeImports",
-    arguments = { vim.api.nvim_buf_get_name(0) },
-  }
-  vim.lsp.buf.execute_command(params)
+	local params = {
+		command = "_typescript.organizeImports",
+		arguments = { vim.api.nvim_buf_get_name(0) },
+	}
+	vim.lsp.buf.execute_command(params)
 end
 
 local mason_registry = require("mason-registry")
@@ -19,112 +19,116 @@ local has_volar, volar = pcall(mason_registry.get_package, "vue-language-server"
 -- npm i -g @vue/typescript-plugin
 local vue_language_server_path
 if has_volar then
-  vue_language_server_path = volar:get_install_path() .. "/node_modules/@vue/language-server"
+	vue_language_server_path = volar:get_install_path() .. "/node_modules/@vue/language-server"
 end
 
 require("mason-lspconfig").setup_handlers({
-  function(server)
-    local server_config = {
-      on_attach = on_attach,
-      capabilities = capabilities,
-    }
-    if server == "rust_analyzer" then
-      return nil
-    end
-    lspconfig[server].setup(server_config)
-  end,
+	function(server)
+		local server_config = {
+			on_attach = on_attach,
+			capabilities = capabilities,
+		}
+		if server == "rust_analyzer" or server == "clangd" or server == "tsserver" or server == "pylsp" then
+			return nil
+		end
+		lspconfig[server].setup(server_config)
+	end,
 })
 
 local rust_config = require("configs.rust_config")
 lspconfig["rust_analyzer"].setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  settings = rust_config,
+	on_attach = on_attach,
+	capabilities = capabilities,
+	settings = rust_config,
 })
 
-lspconfig.volar.setup({})
-
 lspconfig.tsserver.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-  init_options = {
-    plugins = {
-      {
-        name = "@vue/typescript-plugin",
-        location = vue_language_server_path,
-        languages = { "vue" },
-      },
-    },
-  },
-  commands = {
-    OrganizeImports = {
-      organize_imports,
-      description = "Organize Imports",
-    },
-  },
+	on_attach = on_attach,
+	capabilities = capabilities,
+	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+	init_options = {
+		plugins = {
+			{
+				name = "@vue/typescript-plugin",
+				location = vue_language_server_path,
+				languages = { "vue" },
+			},
+		},
+	},
+	commands = {
+		OrganizeImports = {
+			organize_imports,
+			description = "Organize Imports",
+		},
+	},
+})
+
+lspconfig.clangd.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
 })
 
 lspconfig.pylsp.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "python" },
-  settings = {
-    pylsp = {
-      plugins = {
-        pycodestyle = {
-          ignore = { "W391" },
-          maxLineLength = 100,
-        },
-      },
-    },
-  },
+	on_attach = on_attach,
+	capabilities = capabilities,
+	filetypes = { "python" },
+	settings = {
+		pylsp = {
+			plugins = {
+				pycodestyle = {
+					ignore = { "W391" },
+					maxLineLength = 100,
+				},
+			},
+		},
+	},
 })
 
 lspconfig.gopls.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  cmd = { "gopls" },
-  cmd_env = {
-    GOFLAGS = "-tags=test,e2e_test,integration_test,acceptance_test",
-  },
-  filetypes = { "go", "gomod", "gowork", "gotmpl" },
-  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-  settings = {
-    gopls = {
-      completeUnimported = true,
-      usePlaceholders = true,
-      analyses = {
-        unusedparams = true,
-      },
-    },
-  },
+	on_attach = on_attach,
+	capabilities = capabilities,
+	cmd = { "gopls" },
+	cmd_env = {
+		GOFLAGS = "-tags=test,e2e_test,integration_test,acceptance_test",
+	},
+	filetypes = { "go", "gomod", "gowork", "gotmpl" },
+	root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+	settings = {
+		gopls = {
+			completeUnimported = true,
+			usePlaceholders = true,
+			analyses = {
+				unusedparams = true,
+			},
+		},
+	},
 })
 
 lspconfig.jsonls.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  settings = {
-    json = {
-      schemas = require("schemastore").json.schemas(),
-      validate = { enable = true },
-    },
-  },
+	on_attach = on_attach,
+	capabilities = capabilities,
+	settings = {
+		json = {
+			schemas = require("schemastore").json.schemas(),
+			validate = { enable = true },
+		},
+	},
 })
 
 lspconfig.yamlls.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  settings = {
-    yaml = {
-      schemaStore = {
-        -- You must disable built-in schemaStore support if you want to use
-        -- this plugin and its advanced options like `ignore`.
-        enable = false,
-        -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
-        url = "",
-      },
-      schemas = require("schemastore").yaml.schemas(),
-    },
-  },
+	on_attach = on_attach,
+	capabilities = capabilities,
+	settings = {
+		yaml = {
+			schemaStore = {
+				-- You must disable built-in schemaStore support if you want to use
+				-- this plugin and its advanced options like `ignore`.
+				enable = false,
+				-- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+				url = "",
+			},
+			schemas = require("schemastore").yaml.schemas(),
+		},
+	},
 })
