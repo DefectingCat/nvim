4585cdddfd956756f5828104a566fe48308586fd
xfy <xfyxfy@proton.me>
2024-06-14 09:11:27 +0800

    add restore cursor position

--- lua/utils/autocmd.lua
+++ lua/utils/autocmd.lua
@@ -86,3 +86,19 @@ autocmd("TextYankPost", {
 --     require("conform").format { bufnr = args.buf }
 --   end,
 -- })
+
+-- This autocmd will restore cursor position on file open
+autocmd("BufReadPost", {
+	pattern = "*",
+	callback = function()
+		local line = vim.fn.line("'\"")
+		if
+			line > 1
+			and line <= vim.fn.line("$")
+			and vim.bo.filetype ~= "commit"
+			and vim.fn.index({ "xxd", "gitrebase" }, vim.bo.filetype) == -1
+		then
+			vim.cmd('normal! g`"')
+		end
+	end,
+})
