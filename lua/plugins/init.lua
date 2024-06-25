9ab3ad34c1d01f41526722438e22719822dd2224
xfy <xfyxfy@proton.me>
2024-06-25 11:16:39 +0800

    add neogit plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -471,6 +471,16 @@ local plugins = {
 			},
 		},
 	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+			"nvim-telescope/telescope.nvim", -- optional
+		},
+		event = { "VeryLazy" },
+		config = true,
+	},
 
 	--[[ overrides ]]
 	{
