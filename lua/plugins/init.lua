962b27295b6c259a56dac422718b8fae0b1dae70
xfy <i@rua.plus>
2024-07-04 10:45:02 +0800

    fix telescope-fzf-native build

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -260,7 +260,8 @@ local plugins = {
 	},
 	{
 		"nvim-telescope/telescope-fzf-native.nvim",
-		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+		--[[ build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build", ]]
+		build = "make",
 	},
 	{
 		"nvim-pack/nvim-spectre",
