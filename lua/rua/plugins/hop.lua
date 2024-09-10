20400f7596e60a2f1a592901b2e6258aba1481d8
xfy <i@defect.ink>
2024-09-10 10:54:49 +0800

    add basic plugins

---
+++ lua/rua/plugins/hop.lua
@@ -0,0 +1,21 @@
+return {
+  "phaazon/hop.nvim",
+  branch = "v2",
+  config = function()
+    local hop = require("hop")
+    hop.setup({ keys = "etovxqpdygfblzhckisuran" })
+
+    local map = vim.keymap.set
+    map({ "n", "v" }, "f", function()
+      local directions = require("hop.hint").HintDirection
+      hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false })
+    end, { desc = "Hop motion search in current line after cursor" })
+    map({ "n", "v" }, "F", function()
+      local directions = require("hop.hint").HintDirection
+      hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false })
+    end, { desc = "Hop motion search in current line before cursor" })
+    map({ "n", "v" }, "<leader><leader>", function()
+      hop.hint_words({ current_line_only = false })
+    end, { desc = "Hop motion search words after cursor" })
+  end,
+}
---
+++ lua/rua/plugins/oil.lua
@@ -0,0 +1,35 @@
+return {
+  "stevearc/oil.nvim",
+  event = "BufReadPost",
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup({
+      view_options = {
+        show_hidden = true,
+      },
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = false,
+        --[[ ["<C-h>"] = { "actions.select", opts = { horizontal = true } }, ]]
+        ["<C-h>"] = false,
+        ["<C-t>"] = { "actions.select", opts = { tab = true } },
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = false,
+        ["q"] = "actions.close",
+        ["<C-l>"] = false,
+        ["<C-r>"] = { "actions.refresh" },
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = { "actions.cd", opts = { scope = "tab" } },
+        ["gs"] = "actions.change_sort",
+        ["gx"] = "actions.open_external",
+        ["g."] = "actions.toggle_hidden",
+        ["g\\"] = "actions.toggle_trash",
+      },
+      skip_confirm_for_simple_edits = true,
+    })
+  end,
+}
