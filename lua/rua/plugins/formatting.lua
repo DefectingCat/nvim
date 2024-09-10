1cb0046beb2545dd1f7744212de15cb5fb0114ce
xfy <i@defect.ink>
2024-09-10 09:27:46 +0800

    update key

--- lua/rua/plugins/formatting.lua
+++ lua/rua/plugins/formatting.lua
@@ -126,7 +126,7 @@ return {
     vim.keymap.set({ "n", "v" }, "<leader>fm", function()
       conform.format({
         lsp_fallback = true,
-        async = false,
+        -- async = false,
         timeout_ms = 1000,
       })
     end, { desc = "Format file or range (in visual mode)" })
