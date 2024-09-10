961813f79624b8e4b278da917421af252c46e5bb
xfy <i@defect.ink>
2024-09-10 09:25:15 +0800

    change lualine section

--- lua/rua/plugins/lualine.lua
+++ lua/rua/plugins/lualine.lua
@@ -13,11 +13,11 @@ return {
       extensions = { "quickfix", "trouble", "mason", "lazy", "nvim-tree" },
       sections = {
         lualine_x = {
-          {
-            lazy_status.updates,
-            cond = lazy_status.has_updates,
-            color = { fg = "#ff9e64" },
-          },
+          -- {
+          --   lazy_status.updates,
+          --   cond = lazy_status.has_updates,
+          --   color = { fg = "#ff9e64" },
+          -- },
           { "encoding" },
           { "fileformat" },
           { "filetype" },
