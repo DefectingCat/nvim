76e07772447073ffaabf350ea28755ccf4d5988a
xfy <xfyxfy@proton.me>
2024-09-11 11:40:33 +0800

    update hop mappings

--- lua/rua/plugins/hop.lua
+++ lua/rua/plugins/hop.lua
@@ -2,25 +2,37 @@ return {
   "phaazon/hop.nvim",
   branch = "v2",
   keys = {
-    { "f",                desc = "" },
-    { "F",                desc = "" },
-    { "<leader><leader>", desc = "" },
+    {
+      "f",
+      function()
+        local hop = require("hop")
+        local directions = require("hop.hint").HintDirection
+        hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false })
+      end,
+      desc = "Hop motion search in current line after cursor",
+      mode = { "n", "v" },
+    },
+    {
+      "F",
+      function()
+        local hop = require("hop")
+        local directions = require("hop.hint").HintDirection
+        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false })
+      end,
+      desc = "Hop motion search in current line before cursor",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader><leader>",
+      function()
+        local hop = require("hop")
+        hop.hint_words({ current_line_only = false })
+      end,
+      desc = "Hop motion search words after cursor",
+      mode = { "n", "v" },
+    },
   },
-  config = function()
-    local hop = require("hop")
-    hop.setup({ keys = "etovxqpdygfblzhckisuran" })
-
-    local map = vim.keymap.set
-    map({ "n", "v" }, "f", function()
-      local directions = require("hop.hint").HintDirection
-      hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false })
-    end, { desc = "Hop motion search in current line after cursor" })
-    map({ "n", "v" }, "F", function()
-      local directions = require("hop.hint").HintDirection
-      hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false })
-    end, { desc = "Hop motion search in current line before cursor" })
-    map({ "n", "v" }, "<leader><leader>", function()
-      hop.hint_words({ current_line_only = false })
-    end, { desc = "Hop motion search words after cursor" })
-  end,
+  opts = {
+    keys = "etovxqpdygfblzhckisuran",
+  },
 }
