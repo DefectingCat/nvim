20cd0e73d9b2208ab9a106899ea64b88a4b01375
xfy <xfyxfy@proton.me>
2024-09-11 13:17:09 +0800

    update indent-blankline

--- lua/rua/plugins/ui.lua
+++ lua/rua/plugins/ui.lua
@@ -122,6 +122,21 @@ return {
     opts = {
       indent = { char = "│" },
       scope = { enabled = false },
+      exclude = {
+        filetypes = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
+      },
     },
   },
   {
