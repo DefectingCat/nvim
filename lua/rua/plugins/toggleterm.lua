d3185aae49ed1b3aea6b0fafafd8f99391a5376e
xfy <xfyxfy@proton.me>
2024-09-12 17:29:04 +0800

    fix toggle load time

--- lua/rua/plugins/toggleterm.lua
+++ lua/rua/plugins/toggleterm.lua
@@ -4,28 +4,38 @@ return {
     version = "*",
     opts = {},
     keys = function()
-      local Terminal = require("toggleterm.terminal").Terminal
-      local float = Terminal:new({
-        direction = "float",
-        float_opts = {
-          border = "curved",
-        },
-      })
-      -- vim.keymap.set({ "n", "t" }, "<C-\\>", function()
-      --   float:toggle()
-      -- end, { noremap = true, silent = true })
+      local test = function()
+        local Terminal = require("toggleterm.terminal").Terminal
+        local float = Terminal:new({
+          direction = "float",
+          float_opts = {
+            border = "curved",
+          },
+        })
+        float:toggle()
+      end
       return {
         {
           [[<A-i>]],
-          function()
-            float:toggle()
-          end,
+          test,
           desc = "Toggle float terminal",
           mode = { "n", "t" },
           -- { noremap = true, silent = true },
         },
       }
     end,
-    config = function() end,
+    -- config = function()
+    --   local Terminal = require("toggleterm.terminal").Terminal
+    --   local float = Terminal:new({
+    --     direction = "float",
+    --     float_opts = {
+    --       border = "curved",
+    --     },
+    --   })
+    --   vim.keymap.set({ "n", "t" }, "<A-i>", function()
+    --     float:toggle()
+    --   end, { noremap = true, silent = true })
+    -- end,
+    -- keys = { "<A-i>" },
   },
 }
