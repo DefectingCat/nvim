0ffca19aa3c538f6cba705e17d7c84c4e092b190
xfy <xfyxfy@proton.me>
2024-09-12 17:31:47 +0800

    remove comment

--- lua/rua/plugins/toggleterm.lua
+++ lua/rua/plugins/toggleterm.lua
@@ -16,26 +16,5 @@ return {
       end, { noremap = true, silent = true })
     end,
     keys = { "<A-i>" },
-    -- keys = function()
-    --   local test = function()
-    --     local Terminal = require("toggleterm.terminal").Terminal
-    --     local float = Terminal:new({
-    --       direction = "float",
-    --       float_opts = {
-    --         border = "curved",
-    --       },
-    --     })
-    --     float:toggle()
-    --   end
-    --   return {
-    --     {
-    --       [[<A-i>]],
-    --       test,
-    --       desc = "Toggle float terminal",
-    --       mode = { "n", "t" },
-    --       -- { noremap = true, silent = true },
-    --     },
-    --   }
-    -- end,
   },
 }
