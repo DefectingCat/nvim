f87c4a0b99f8e2fb39c7e673dcfe3a12ec1d49b7
xfy <xfyxfy@proton.me>
2024-09-12 17:31:27 +0800

    fix toggleterm

--- lua/rua/plugins/toggleterm.lua
+++ lua/rua/plugins/toggleterm.lua
@@ -3,39 +3,39 @@ return {
     "akinsho/toggleterm.nvim",
     version = "*",
     opts = {},
-    keys = function()
-      local test = function()
-        local Terminal = require("toggleterm.terminal").Terminal
-        local float = Terminal:new({
-          direction = "float",
-          float_opts = {
-            border = "curved",
-          },
-        })
-        float:toggle()
-      end
-      return {
-        {
-          [[<A-i>]],
-          test,
-          desc = "Toggle float terminal",
-          mode = { "n", "t" },
-          -- { noremap = true, silent = true },
+    config = function()
+      local Terminal = require("toggleterm.terminal").Terminal
+      local float = Terminal:new({
+        direction = "float",
+        float_opts = {
+          border = "curved",
         },
-      }
+      })
+      vim.keymap.set({ "n", "t" }, "<A-i>", function()
+        float:toggle()
+      end, { noremap = true, silent = true })
     end,
-    -- config = function()
-    --   local Terminal = require("toggleterm.terminal").Terminal
-    --   local float = Terminal:new({
-    --     direction = "float",
-    --     float_opts = {
-    --       border = "curved",
-    --     },
-    --   })
-    --   vim.keymap.set({ "n", "t" }, "<A-i>", function()
+    keys = { "<A-i>" },
+    -- keys = function()
+    --   local test = function()
+    --     local Terminal = require("toggleterm.terminal").Terminal
+    --     local float = Terminal:new({
+    --       direction = "float",
+    --       float_opts = {
+    --         border = "curved",
+    --       },
+    --     })
     --     float:toggle()
-    --   end, { noremap = true, silent = true })
+    --   end
+    --   return {
+    --     {
+    --       [[<A-i>]],
+    --       test,
+    --       desc = "Toggle float terminal",
+    --       mode = { "n", "t" },
+    --       -- { noremap = true, silent = true },
+    --     },
+    --   }
     -- end,
-    -- keys = { "<A-i>" },
   },
 }
