284b65a5c94c0987bcd723f5f785d629c15813cd
xfy <xfyxfy@proton.me>
2024-09-13 13:19:12 +0800

    move nvim-tree side to right

--- lua/rua/plugins/nvim-tree.lua
+++ lua/rua/plugins/nvim-tree.lua
@@ -1,7 +1,13 @@
 return {
   "nvim-tree/nvim-tree.lua",
   dependencies = "nvim-tree/nvim-web-devicons",
-  event = "VeryLazy",
+  -- event = "VeryLazy",
+  cmd = {
+    "NvimTreeFindFileToggle",
+  },
+  keys = {
+    { "<leader>e", "<cmd>NvimTreeFindFileToggle<CR>", desc = "Toggle file explorer on current file" },
+  },
   config = function()
     local nvimtree = require("nvim-tree")
 
@@ -14,6 +20,7 @@ return {
         width = 35,
         relativenumber = false,
         number = false,
+        side = "right",
       },
       update_focused_file = {
         enable = true,
@@ -52,13 +59,5 @@ return {
         ignore = false,
       },
     })
-
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
-
-    -- keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-    keymap.set("n", "<leader>e", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-    -- keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-    -- keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
   end,
 }
