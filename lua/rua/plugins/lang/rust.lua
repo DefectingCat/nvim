9243a785463eca4b117eaf78b1cd1c8d8438854a
xfy <xfyxfy@proton.me>
2024-09-14 15:05:54 +0800

    update rust configuration

--- lua/rua/plugins/lang/rust.lua
+++ lua/rua/plugins/lang/rust.lua
@@ -1,3 +1,5 @@
+local rust_settings = require("rua.config.rust-analyzer")
+
 return {
   {
     "Saecki/crates.nvim",
@@ -27,32 +29,11 @@ return {
           vim.keymap.set("n", "<leader>ca", function()
             vim.cmd.RustLsp("codeAction")
           end, { desc = "Code Action", buffer = bufnr })
-          vim.keymap.set("n", "<leader>dr", function()
+          vim.keymap.set("n", "<leader>da", function()
             vim.cmd.RustLsp("debuggables")
           end, { desc = "Rust Debuggables", buffer = bufnr })
         end,
-        default_settings = {
-          -- rust-analyzer language server configuration
-          ["rust-analyzer"] = {
-            cargo = {
-              allFeatures = true,
-              loadOutDirsFromCheck = true,
-              buildScripts = {
-                enable = true,
-              },
-            },
-            -- Add clippy lints for Rust.
-            checkOnSave = true,
-            procMacro = {
-              enable = true,
-              ignored = {
-                ["async-trait"] = { "async_trait" },
-                ["napi-derive"] = { "napi" },
-                ["async-recursion"] = { "async_recursion" },
-              },
-            },
-          },
-        },
+        default_settings = rust_settings,
       },
     },
     config = function(_, opts)
