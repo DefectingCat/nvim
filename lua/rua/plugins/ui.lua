d096a239b07aef654cfc3f3ed1d348778f1221ba
xfy <xfyxfy@proton.me>
2024-09-14 15:59:01 +0800

    update vim-visual-multi mapping

--- lua/rua/plugins/ui.lua
+++ lua/rua/plugins/ui.lua
@@ -43,7 +43,14 @@ return {
   },
   {
     "mg979/vim-visual-multi",
-    keys = { "<C-n>", desc = "Start vim visual multi" },
+    keys = {
+      {
+        "<C-n>",
+        "<Plug>(VM-Find-Subword-Under)",
+        desc = "Start vim visual multi",
+        mode = { "n", "v" },
+      },
+    },
   },
   {
     "mistricky/codesnap.nvim",
