2660f9e317daa38462c233bc020ce918a236fb43
xfy <xfyxfy@proton.me>
2024-09-27 13:24:16 +0800

    update telescope
    remove bufferline

--- lua/rua/plugins/telescope.lua
+++ lua/rua/plugins/telescope.lua
@@ -11,7 +11,6 @@ return {
     { "<leader>fh", "<cmd>Telescope search_history<cr>", desc = "Find search history" },
     { "<leader>gm", "<cmd>Telescope git_commits<CR>", desc = "Git commits" },
     {
-
       "<leader>fc",
       function()
         require("telescope.builtin").command_history()
@@ -42,7 +41,7 @@ return {
       end,
       desc = "Find Diagnostics",
     },
-    { "<leader>b", "<cmd>Telescope buffers<CR>", desc = "telescope find buffers" },
+    { "<leader>b", "<cmd>Telescope buffers initial_mode=normal<CR>", desc = "telescope find buffers" },
   },
   dependencies = {
     "nvim-lua/plenary.nvim",
@@ -69,12 +68,41 @@ return {
       defaults = {
         path_display = { "smart" },
         mappings = {
+          n = {
+            ["d"] = require("telescope.actions").delete_buffer,
+          },
           i = {
             ["<C-p>"] = actions.move_selection_previous, -- move to prev result
             ["<C-n>"] = actions.move_selection_next, -- move to next result
             ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
             ["<C-t>"] = trouble_telescope.open,
           },
+        },
+      },
+      pickers = {
+        buffers = {
+          theme = "ivy",
+        },
+        git_files = {
+          theme = "ivy",
+        },
+        oldfiles = {
+          theme = "ivy",
+        },
+        find_files = {
+          theme = "ivy",
+        },
+        colorscheme = {
+          theme = "ivy",
+        },
+        marks = {
+          theme = "ivy",
+        },
+        lsp_document_symblos = {
+          theme = "ivy",
+        },
+        lsp_dynamic_workspace_symbols = {
+          theme = "ivy",
         },
       },
     })
