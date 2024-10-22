851587f11e90bac8208d872efc2636ebcddb3e19
xfy <i@rua.plus>
2024-10-22 17:38:39 +0800

    add neovide configuration

--- lua/rua/core/init.lua
+++ lua/rua/core/init.lua
@@ -3,6 +3,10 @@ require("rua.core.keymaps")
 require("rua.core.usercmd")
 require("rua.core.autocmd")
 
+if vim.g.neovide then
+  require("rua.core.neovide")
+end
+
 -- To appropriately highlight codefences returned from denols, you will
 -- need to augment vim.g.markdown_fenced languages in your init.lua. Example:
 vim.g.markdown_fenced_languages = {
