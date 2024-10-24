d7aaa47fcac3ff7727bc889b9b3f715e15a8c388
xfy <i@rua.plus>
2024-10-24 16:09:06 +0800

    add mason install all manually

--- lua/rua/plugins/lsp/mason.lua
+++ lua/rua/plugins/lsp/mason.lua
@@ -1,14 +1,14 @@
 return {
   "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
+  -- dependencies = {
+  --   "williamboman/mason-lspconfig.nvim",
+  --   "WhoIsSethDaniel/mason-tool-installer.nvim",
+  -- },
   event = "VeryLazy",
   config = function()
     local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
-    local mason_tool_installer = require("mason-tool-installer")
+    -- local mason_lspconfig = require("mason-lspconfig")
+    -- local mason_tool_installer = require("mason-tool-installer")
 
     -- enable mason and configure icons
     mason.setup({
@@ -21,48 +21,47 @@ return {
       },
     })
 
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      ensure_installed = {
-        "gopls",
-        "lua_ls",
-        "rust_analyzer",
-        "html",
-        "volar", -- vue
-        "vtsls", -- typescript
-        "tailwindcss",
-        "eslint",
-        "cssls",
-        "cssmodules_ls",
-        "jsonls",
-        "yamlls",
-        "docker_compose_language_service",
-        "dockerls",
-        "bashls",
-        "clangd",
-        "lemminx", -- xml svg
-        "denols",
-      },
-    })
-
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "pylint",
-        "stylua",
-        "shfmt",
-        "goimports",
-        "gofumpt",
-        "golines",
-        "gomodifytags",
-        "impl", -- go
-        "clang-format",
-        "taplo", -- toml
-        "delve", -- golang debug adapter
-      },
-    })
+    -- create install all command
+    local ensure_installed = {
+      "gopls",
+      "lua-language-server",
+      "rust-analyzer",
+      "html-lsp",
+      "vue-language-server", -- vue
+      "vtsls", -- typescript
+      "tailwindcss-language-server",
+      "eslint-lsp",
+      "css-lsp",
+      "cssmodules-language-server",
+      "json-lsp",
+      "yaml-language-server",
+      "docker-compose-language-service",
+      "dockerfile-language-server",
+      "bash-language-server",
+      "clangd",
+      "lemminx", -- xml svg
+      "deno",
+      -- tools
+      "prettier", -- prettier formatter
+      "stylua", -- lua formatter
+      "isort", -- python formatter
+      -- "black", -- python formatter
+      "pylint",
+      "shfmt",
+      "goimports",
+      "gofumpt",
+      "golines",
+      "gomodifytags",
+      "impl", -- go
+      "clang-format",
+      "taplo", -- toml
+      "delve", -- golang debug adapter
+    }
+    -- Create user command to synchronously install all Mason tools in `opts.ensure_installed`.
+    vim.api.nvim_create_user_command("MasonInstallAll", function()
+      for _, tool in ipairs(ensure_installed) do
+        vim.cmd("MasonInstall " .. tool)
+      end
+    end, {})
   end,
 }
