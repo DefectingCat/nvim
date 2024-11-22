b366dd8e8c1a4aa6a89c8f8e497c7c65909f3b77
xfy <xfyxfy@proton.me>
2024-11-22 11:58:54 +0800

    Revert 1 commits
    77fc5d1 'change default insert mode'

--- lua/rua/plugins/telescope.lua
+++ lua/rua/plugins/telescope.lua
@@ -44,7 +44,7 @@ return {
     {
       "<leader>b",
       function()
-        require("telescope.builtin").buffers({ sort_lastused = true })
+        require("telescope.builtin").buffers({ sort_lastused = true, initial_mode = "normal" })
       end,
       desc = "Buffers",
     },
