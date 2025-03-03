6800ba2298933fd50dc5b40ba6556b0fc06285ec
xfy <i@rua.plus>
2025-03-03 08:42:57 +0800

    update colorscheme

--- init.lua
+++ init.lua
@@ -2,5 +2,7 @@ require("rua.core")
 require("rua.lazy")
 
 -- vim.cmd.colorscheme("ex-lackluster-hack")
+vim.cmd.colorscheme("ex-catppuccin-macchiato")
 -- vim.cmd.colorscheme("ex-rei")
-vim.cmd.colorscheme("rei")
+-- vim.cmd.colorscheme("rei")
+-- vim.cmd.colorscheme("catppuccin")
--- lua/rua/plugins/colorscheme.lua
+++ lua/rua/plugins/colorscheme.lua
@@ -52,66 +52,122 @@ local init_lackluster = function()
 end
 
 return {
-  -- {
-  --   "catppuccin/nvim",
-  --   name = "catppuccin",
-  --   priority = 1000,
-  --   lazy = false,
-  --   config = function()
-  --     require("catppuccin").setup({
-  --       flavour = "mocha", -- latte, frappe, macchiato, mocha
-  --       background = { -- :h background
-  --         light = "latte",
-  --         dark = "mocha",
-  --       },
-  --       transparent_background = true, -- disables setting the background color.
-  --       show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-  --       term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-  --       dim_inactive = {
-  --         enabled = false, -- dims the background color of inactive window
-  --         shade = "dark",
-  --         percentage = 0.15, -- percentage of the shade to apply to the inactive window
-  --       },
-  --       no_italic = false, -- Force no italic
-  --       no_bold = false, -- Force no bold
-  --       no_underline = false, -- Force no underline
-  --       styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-  --         comments = { "italic" }, -- Change the style of comments
-  --         conditionals = { "italic" },
-  --         loops = {},
-  --         functions = {},
-  --         keywords = {},
-  --         strings = {},
-  --         variables = {},
-  --         numbers = {},
-  --         booleans = {},
-  --         properties = {},
-  --         types = {},
-  --         operators = {},
-  --         -- miscs = {}, -- Uncomment to turn off hard-coded styles
-  --       },
-  --       color_overrides = {},
-  --       custom_highlights = {},
-  --       default_integrations = true,
-  --       integrations = {
-  --         cmp = true,
-  --         gitsigns = true,
-  --         nvimtree = true,
-  --         treesitter = true,
-  --         notify = false,
-  --         mini = {
-  --           enabled = true,
-  --           indentscope_color = "",
-  --         },
-  --         -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-  --       },
-  --     })
-  --     -- vim.cmd.colorscheme("catppuccin")
-  --   end,
-  -- },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    -- priority = 1000,
+    -- lazy = true,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "macchiato", -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+          light = "latte",
+          dark = "mocha",
+        },
+        transparent_background = true, -- disables setting the background color.
+        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+        term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+        dim_inactive = {
+          enabled = false, -- dims the background color of inactive window
+          shade = "dark",
+          percentage = 0.15, -- percentage of the shade to apply to the inactive window
+        },
+        no_italic = false, -- Force no italic
+        no_bold = false, -- Force no bold
+        no_underline = false, -- Force no underline
+        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { "italic" }, -- Change the style of comments
+          conditionals = { "italic" },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+          -- miscs = {}, -- Uncomment to turn off hard-coded styles
+        },
+        color_overrides = {
+          rosewater = "#f4dbd6",
+          flamingo = "#f0c6c6",
+          pink = "#f5bde6",
+          mauve = "#B9AEE6",
+          red = "#A87692",
+          maroon = "#FD9491",
+          peach = "#f5a97f",
+          yellow = "#FCD0A1",
+          green = "#809FA1",
+          teal = "#8bd5ca",
+          sky = "#91d7e3",
+          sapphire = "#7dc4e4",
+          blue = "#91B8E9",
+          lavender = "#b7bdf8",
+          text = "#DAD0E9",
+          subtext1 = "#b8c0e0",
+          subtext0 = "#a5adcb",
+          overlay2 = "#939ab7",
+          overlay1 = "#8087a2",
+          overlay0 = "#6e738d",
+          surface2 = "#5b6078",
+          surface1 = "#494d64",
+          surface0 = "#363a4f",
+          base = "#24273a",
+          mantle = "#1e2030",
+          crust = "#181926",
+        },
+        custom_highlights = {},
+        default_integrations = true,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          alpha = true,
+          mini = {
+            enabled = true,
+            indentscope_color = "",
+          },
+          mason = true,
+          markdown = true,
+          dap = true,
+          dap_ui = true,
+          which_key = false,
+          native_lsp = {
+            enabled = true,
+            virtual_text = {
+              errors = { "italic" },
+              hints = { "italic" },
+              warnings = { "italic" },
+              information = { "italic" },
+              ok = { "italic" },
+            },
+            underlines = {
+              errors = { "underline" },
+              hints = { "underline" },
+              warnings = { "underline" },
+              information = { "underline" },
+              ok = { "underline" },
+            },
+            inlay_hints = {
+              background = true,
+            },
+          },
+          telescope = {
+            enabled = true,
+            -- style = "nvchad"
+          },
+          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+        },
+      })
+      -- vim.cmd.colorscheme("catppuccin")
+    end,
+  },
   {
     "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons", "wheat-thin-wiens/rei.nvim" },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     event = "VimEnter",
     config = function()
       local lualine = require("lualine")
@@ -121,7 +177,7 @@ return {
         options = {
           -- theme = "catppuccin",
           -- theme = "lackluster",
-          theme = "rei",
+          -- theme = "rei",
           component_separators = { left = "", right = "" },
           section_separators = { left = "", right = "" },
         },
@@ -160,30 +216,30 @@ return {
     -- priority = 1000,
     -- init = init_lackluster,
   },
-  {
-    -- "wheat-thin-wiens/rei.nvim",
-    "DefectingCat/rei.nvim",
-    priority = 1000,
-    -- lazy = true,
-    config = function()
-      require("rei").setup({
-        styles = {
-          comments = { italic = true },
-          keywords = {},
-          identifiers = {},
-          functions = { italic = true, bold = true },
-          variables = {},
-          booleans = {},
-          loops = { italic = true },
-        },
-        integrations = {
-          telescope = "borderless", -- 'borderless' theme also available, leave as true for default theme
-        },
-        transparency = true, -- Enables / Disables background transparency
-        terminal_colors = true,
-        highlight_overrides = {},
-      })
-      -- vim.cmd.colorscheme("rei")
-    end,
-  },
+  -- {
+  --   -- "wheat-thin-wiens/rei.nvim",
+  --   "DefectingCat/rei.nvim",
+  --   priority = 1000,
+  --   -- lazy = true,
+  --   config = function()
+  --     require("rei").setup({
+  --       styles = {
+  --         comments = { italic = true },
+  --         keywords = {},
+  --         identifiers = {},
+  --         functions = { italic = true, bold = true },
+  --         variables = {},
+  --         booleans = {},
+  --         loops = { italic = true },
+  --       },
+  --       integrations = {
+  --         telescope = "borderless", -- 'borderless' theme also available, leave as true for default theme
+  --       },
+  --       transparency = true, -- Enables / Disables background transparency
+  --       terminal_colors = true,
+  --       highlight_overrides = {},
+  --     })
+  --     -- vim.cmd.colorscheme("rei")
+  --   end,
+  -- },
 }
