5c120f217cb62425b683997537b3cc943f4a8f6a
xfy <i@rua.plus>
2025-06-21 12:42:40 +0800

    update avante

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,88 +1,89 @@
-return {
-  "yetone/avante.nvim",
-  -- event = "VeryLazy",
-  lazy = true,
-  version = false, -- Never set this value to "*"! Never!
-  keys = {
-    "<leader>aa",
-    "<leader>at",
-    "<leader>ar",
-    "<leader>af",
-    "<leader>an",
-  },
-  cmd = {
-    "AvanteAsk",
-    "AvanteBuild",
-    "AvanteChat",
-    "AvanteClear",
-    "AvanteEdit",
-    "AvanteFocus",
-    "AvanteRefresh",
-    "AvanteStop",
-    "AvanteSwitchProvider",
-    "AvanteShowRepoMap",
-    "AvanteToggle",
-    "AvanteModels",
-  },
-  opts = {
-    provider = "doubao",
-    providers = {
-      deepseek = {
-        __inherited_from = "openai",
-        api_key_name = "DEEPSEEK_API_KEY",
-        endpoint = "https://api.deepseek.com",
-        model = "deepseek-coder",
-        max_tokens = 256000,
-      },
-      doubao = {
-        __inherited_from = "openai",
-        api_key_name = "DOUBAO_API_KEY",
-        endpoint = "https://ark.cn-beijing.volces.com/api/v3",
-        model = "doubao-seed-1-6-250615",
-        max_tokens = 8192,
-        max_completion_tokens = 12288,
-      },
-    },
-  },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "stevearc/dressing.nvim",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
-    -- "echasnovski/mini.pick", -- for file_selector provider mini.pick
-    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-    "ibhagwan/fzf-lua", -- for file_selector provider fzf
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    -- "zbirenbaum/copilot.lua", -- for providers='copilot'
-    {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      -- event = "VeryLazy",
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = true,
-        },
-      },
-    },
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      "MeanderingProgrammer/render-markdown.nvim",
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
-    },
-  },
-}
+-- return {
+--   "yetone/avante.nvim",
+--   -- event = "VeryLazy",
+--   lazy = true,
+--   version = false, -- Never set this value to "*"! Never!
+--   keys = {
+--     "<leader>aa",
+--     "<leader>at",
+--     "<leader>ar",
+--     "<leader>af",
+--     "<leader>an",
+--   },
+--   cmd = {
+--     "AvanteAsk",
+--     "AvanteBuild",
+--     "AvanteChat",
+--     "AvanteClear",
+--     "AvanteEdit",
+--     "AvanteFocus",
+--     "AvanteRefresh",
+--     "AvanteStop",
+--     "AvanteSwitchProvider",
+--     "AvanteShowRepoMap",
+--     "AvanteToggle",
+--     "AvanteModels",
+--   },
+--   opts = {
+--     provider = "doubao",
+--     providers = {
+--       deepseek = {
+--         __inherited_from = "openai",
+--         api_key_name = "DEEPSEEK_API_KEY",
+--         endpoint = "https://api.deepseek.com",
+--         model = "deepseek-coder",
+--         max_tokens = 256000,
+--       },
+--       doubao = {
+--         __inherited_from = "openai",
+--         api_key_name = "DOUBAO_API_KEY",
+--         endpoint = "https://ark.cn-beijing.volces.com/api/v3",
+--         model = "doubao-seed-1-6-250615",
+--         max_tokens = 8192,
+--         max_completion_tokens = 12288,
+--       },
+--     },
+--   },
+--   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+--   build = "make",
+--   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+--   dependencies = {
+--     "nvim-treesitter/nvim-treesitter",
+--     "stevearc/dressing.nvim",
+--     "nvim-lua/plenary.nvim",
+--     "MunifTanjim/nui.nvim",
+--     --- The below dependencies are optional,
+--     -- "echasnovski/mini.pick", -- for file_selector provider mini.pick
+--     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+--     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+--     "ibhagwan/fzf-lua", -- for file_selector provider fzf
+--     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+--     -- "zbirenbaum/copilot.lua", -- for providers='copilot'
+--     {
+--       -- support for image pasting
+--       "HakonHarnes/img-clip.nvim",
+--       -- event = "VeryLazy",
+--       opts = {
+--         -- recommended settings
+--         default = {
+--           embed_image_as_base64 = false,
+--           prompt_for_file_name = false,
+--           drag_and_drop = {
+--             insert_mode = true,
+--           },
+--           -- required for Windows users
+--           use_absolute_path = true,
+--         },
+--       },
+--     },
+--     {
+--       -- Make sure to set this up properly if you have lazy=true
+--       "MeanderingProgrammer/render-markdown.nvim",
+--       opts = {
+--         file_types = { "markdown", "Avante" },
+--       },
+--       ft = { "markdown", "Avante" },
+--     },
+--   },
+-- }
+return {}
