e6c3b7f18d17a3c3473d9c1653a3c8b403ed7c50
xfy <i@rua.plus>
2026-02-24 11:23:22 +0800

    Configure lualine with custom settings
    - Customize component and section separators
    
    - Configure lualine sections (c, x, y, z)
    
    - Comment out theme and extension configurations

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -8,19 +8,24 @@ return {
       component_separators = { left = "", right = "" },
       section_separators = { left = "", right = "" },
     },
-    extensions = { "quickfix", "trouble", "mason", "lazy" },
+    -- extensions = { "quickfix", "trouble", "mason", "lazy" },
     sections = {
+      lualine_c = {
+        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+        "filename",
+        -- { LazyVim.lualine.pretty_path() },
+      },
       lualine_x = {
         { "encoding" },
         { "fileformat" },
         { "filetype" },
       },
-      -- lualine_y = {
-      --   { "progress", color = { bg = "#de9aa3", fg = "#000000" } },
-      -- },
-      -- lualine_z = {
-      --   { "location", color = { bg = "#eac8c7" } },
-      -- },
+      lualine_y = {
+        { "progress" },
+      },
+      lualine_z = {
+        { "location" },
+      },
     },
   },
 }
