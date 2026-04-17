ba592cc78018fa746de364164a9b987c85741ab2
xfy <i@rua.plus>
2026-04-17 10:46:19 +0800

    Update: 隐藏垂直窗口分隔线 (fillchars 设置)

--- lua/options.lua
+++ lua/options.lua
@@ -6,6 +6,7 @@ local o = vim.o
 o.cursorline = true -- 启用 cursorline
 o.cursorlineopt = "both" -- 同时高亮行和行号
 vim.opt.clipboard = "unnamedplus"
+vim.opt.fillchars = { vert = " " } -- 隐藏垂直窗口分隔线
 vim.o.autoread = true -- 全局启用自动读取
 vim.opt.tabstop = 4
 
